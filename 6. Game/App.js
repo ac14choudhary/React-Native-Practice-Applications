1986ea7dbce7407b3bc90474dca26e5d353ecaf5
ac14choudhary <acayush14@gmail.com>
2020-12-08 05:48:00 +0530

    5th - adding some more styling

--- 6. Game/App.js
+++ 6. Game/App.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import {TouchableOpacity, StyleSheet, View, StatusBar} from 'react-native';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
-} from 'react-native';
-
+  Header,
+  Container,
+  Card,
+  Body,
+  H1,
+  H3,
+  Button,
+  Content,
+} from 'native-base';
 export default function App() {
   return <View></View>;
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  grid: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 20,
+  },
+  box: {
+    width: '33%',
+    marginBottom: 6,
+  },
+  card: {
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    color: 'white',
+    backgroundColor: '#4652b3',
+    paddingVertical: 10,
+  },
+});
