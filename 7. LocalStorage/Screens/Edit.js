5bca9cfe3ece5167348e5cfe7898650685419190
ac14choudhary <acayush14@gmail.com>
2020-12-12 01:11:09 +0530

    9th - Established navigation tagsand screens

---
+++ 7. LocalStorage/Screens/Edit.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default function Edit() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Edit Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#1b262c',
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  heading: {
+    textAlign: 'center',
+    color: '#00b7c2',
+    marginHorizontal: 5,
+    marginTop: 50,
+    marginBottom: 20,
+  },
+  formItem: {
+    marginBottom: 20,
+  },
+});
