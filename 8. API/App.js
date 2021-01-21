b64e6f056a325022dac05672492c95e89b34ad34
ac14choudhary <acayush14@gmail.com>
2021-01-21 10:07:22 +0530

    25th - started redux

--- 8. API/App.js
+++ 8. API/App.js
@@ -33,7 +33,7 @@ export default function App() {
 
   if (!details) {
     return (
-      <View>
+      <View style={styles.container}>
         <ActivityIndicator
           style={styles.Indicator}
           color="green"
