aaad1c6034ade2d00fe71801553194ac7a6829d9
ac14choudhary <acayush14@gmail.com>
2021-02-09 02:28:00 +0530

    27th - redux started

--- 9. REDUX/src/App.js
+++ 9. REDUX/src/App.js
@@ -11,7 +11,7 @@ import {
 export default function App() {
   return (
     <View>
-      <Text></Text>
+      <Text>hello</Text>
     </View>
   );
 }
---
+++ 9. REDUX/src/action/list.js
@@ -0,0 +1,3 @@
+import {ADD_SEASON,REMOVE_SESON,MARK_COMPLETE} from "./action.type"
+
+export const addSeasons = (season) => ()
