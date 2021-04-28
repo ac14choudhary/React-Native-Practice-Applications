126bbb6fc463c0a773a5d20f2e407e9113615d1a
ac14choudhary <acayush14@gmail.com>
2021-04-29 04:17:25 +0530

    35rd - horizontal Scroll

--- 12. Payment_gateway/App.js
+++ 12. Payment_gateway/App.js
@@ -6,12 +6,15 @@ import {
   TouchableOpacity,
   TextInput,
   Dimensions,
+  ScrollView,
   Keyboard,
 } from 'react-native';
 import RazorpayCheckout from 'react-native-razorpay';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
+
+const size = width / 4;
 
 export default function App() {
   const [name, setName] = useState('');
@@ -24,11 +27,14 @@ export default function App() {
       image: 'https://i.imgur.com/3g7nmJC.png',
       currency: 'INR',
       key: 'rzp_test_qD72jCTrv9NMOe', // Your api key
-      amount: {amount},
-      name: {name},
+
+      amount: amount * 100,
+      name: name,
+
       prefill: {
         email: 'void@razorpay.com',
-        contact: {phone},
+        contact: phone,
+
         name: 'Razorpay Software',
       },
       theme: {color: '#121212'},
@@ -41,6 +47,12 @@ export default function App() {
       .catch(error => {
         // handle failure
         alert(`Error: ${error.code} | ${error.description}`);
+      })
+      .finally(() => {
+        console.log('asasas');
+        setAmount('');
+        setName('');
+        setPhone('');
       });
   };
 
@@ -52,65 +64,170 @@ export default function App() {
         alignItems: 'center',
         backgroundColor: 'white',
       }}>
-      <TextInput
+      <View
         style={{
           justifyContent: 'center',
+          flex: 1,
           alignItems: 'center',
-          backgroundColor: '#dddddd',
-          width: width / 1.2,
-          height: 64,
-          color: 'black',
-          paddingHorizontal: 15,
-          borderRadius: 10,
-          marginBottom: 10,
-        }}
-        placeholder="Enter your full name"
-        onChangeText={value => setName(value)}></TextInput>
-
-      <TextInput
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#dddddd',
-          width: width / 1.2,
-          height: 64,
-          color: 'black',
-          paddingHorizontal: 15,
-          borderRadius: 10,
-          marginBottom: 10,
-        }}
-        placeholder="Enter your contact no."
-        onChangeText={value => setPhone(value)}></TextInput>
-
-      <TextInput
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#dddddd',
-          width: width / 1.2,
-          height: 64,
-          color: 'black',
-          paddingHorizontal: 15,
-          borderRadius: 10,
-          marginBottom: 10,
-        }}
-        placeholder="Enter the amount to pay"
-        onChangeText={value => setAmount(value)}></TextInput>
-      <TouchableOpacity
-        onPress={Payment}
-        style={{
-          justifyContent: 'center',
-          backgroundColor: '#72BA54',
-          width: width / 2,
-          height: 50,
-          paddingHorizontal: 15,
-          borderRadius: 10,
-          marginBottom: 10,
+          backgroundColor: 'black',
         }}>
-        <Text style={{alignSelf: 'center', color: 'white'}}>Make payment</Text>
-      </TouchableOpacity>
+        <ScrollView
+          contentContainerStyle={styles.Container}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}>
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 1"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 2"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 4"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 5"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 6"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 7"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 8"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 9"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 10"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 11"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 12"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 13"
+            value={name}></TextInput>
+
+          <TextInput
+            style={styles.scrollBox}
+            placeholder="place 14"
+            value={name}></TextInput>
+        </ScrollView>
+      </View>
+      <View
+        style={{
+          justifyContent: 'center',
+          flex: 4,
+          alignItems: 'center',
+          backgroundColor: 'white',
+        }}>
+        <TextInput
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#dddddd',
+            width: width / 1.2,
+            height: 64,
+            color: 'black',
+            paddingHorizontal: 15,
+            borderRadius: 10,
+            marginBottom: 10,
+          }}
+          placeholder="Enter your full name"
+          value={name}
+          onChangeText={value => setName(value)}></TextInput>
+
+        <TextInput
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#dddddd',
+            width: width / 1.2,
+            height: 64,
+            color: 'black',
+            paddingHorizontal: 15,
+            borderRadius: 10,
+            marginBottom: 10,
+          }}
+          placeholder="Enter your contact no."
+          value={phone}
+          onChangeText={value => setPhone(value)}></TextInput>
+
+        <TextInput
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#dddddd',
+            width: width / 1.2,
+            height: 64,
+            color: 'black',
+            paddingHorizontal: 15,
+            borderRadius: 10,
+            marginBottom: 10,
+          }}
+          placeholder="Enter the amount to pay"
+          value={amount}
+          onChangeText={value => setAmount(value)}></TextInput>
+        <TouchableOpacity
+          onPress={Payment}
+          style={{
+            justifyContent: 'center',
+            backgroundColor: '#72BA54',
+            width: width / 2,
+            height: 50,
+            paddingHorizontal: 15,
+            borderRadius: 10,
+            marginBottom: 10,
+          }}>
+          <Text style={{alignSelf: 'center', color: 'white'}}>
+            Make payment
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  scrollBox: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#dddddd',
+    width: size,
+    height: size,
+    color: 'black',
+    paddingHorizontal: 15,
+    borderRadius: 100,
+    marginLeft: 20,
+  },
+  Container: {
+    alignItems: 'center',
+  },
+});
